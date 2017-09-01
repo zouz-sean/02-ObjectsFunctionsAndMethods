ea3a74a91895146f236f8d49cb92425656a23861
David Fisher <fisherds@rose-hulman.edu>
2017-09-01 13:16:14 -0400

    Minor grammatical updates

--- src/m5r_using_rosegraphics_objects.py
+++ src/m5r_using_rosegraphics_objects.py
@@ -30,7 +30,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #     c. What is the default height of a RoseWindow?
 #        (Type the code shown in your answer above within main to see if PyCharm will help you figure out the answer.
-#         Hint: After you type the line of code, put your cursor within the () area and PyCharm has a popup.)
+#         Hint: After you type the ( in the line of code, if you wait a moment PyCharm will add the ) and has a popup.)
 #            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
 #     d. Write a line of code that construct a RoseWindow object whose height is 100 with any width you choose.
