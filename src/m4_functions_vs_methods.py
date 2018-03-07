69cca615596c084dbfa1f8067317224b52351770
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-07 07:38:57 -0500

    Cosmetic changes to the instructions. No changes to content.

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -1,16 +1,18 @@
 """
 Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
-  -- what is similar
+  -- what is similar, and
   -- how they differ.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-########################################################################
+###############################################################################
 #
 # TODO: 2.
 #   READ this comment, ASKING QUESTIONS as needed to understand it.
+#
+# Part 1:  CONSTRUCTING objects, applying ** METHODS ** to them:
 #
 #   For objects that are CONSTRUCTED, we use the DOT notation
 #   to ask them to do things or to change their characteristics.
@@ -33,9 +35,13 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   The statement
 #        nadia.forward(100)
-#   CALLS the   forward   METHOD on nadia, sending that method the number 100 and making the method run (execute).
+#   CALLS the   forward   METHOD on nadia, sending that method the number 100
+#   and making the method run (execute).
 #
-#   We   ** define **   FUNCTIONS to give NAMES to blocks of code (note FUNCTIONS and METHODS are different).
+# Part 2:  Defining and calling ** FUNCTIONS **.
+#
+#   We   ** define **   FUNCTIONS to give NAMES to blocks of code
+#   (note FUNCTIONS and METHODS are different).
 #   For example:
 #
 #      def turtle3():
@@ -44,21 +50,20 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #          maja.paint_bucket = rg.PaintBucket('black')
 #            ...
 #
-#   We   ** call **   FUNCTIONS (that is, we make them run) in a way that is similar to how we call METHODS,
+#   We   ** call **   FUNCTIONS (that is, we make them run)
+#   in a way that is similar to how we call METHODS,
 #   but WITHOUT the DOT notation.  For example
 #      def main():
 #          turtle3()
 #
-#   Run this module and review the existing code.  Can you identify the functions calls and methods calls?
+#   Run this module and review the existing code.
+#   Can you identify the functions calls and methods calls?
 #
-#   When you believe you understand the differences and similarities
-#   between calling a FUNCTION and calling a METHOD, change the above TO DO to DONE.
+#   When you believe you understand the differences and similarities between
+#   calling a FUNCTION and calling a METHOD, change the above TO-DO to DONE.
 #
-########################################################################
+###############################################################################
 
-# ----------------------------------------------------------------------
-# There are MORE TODOs further down in this module!
-# ----------------------------------------------------------------------
 
 import rosegraphics as rg
 
@@ -71,20 +76,18 @@ def main():
     """
     window = rg.TurtleWindow()
 
-    # Make the animation go much faster.
-    #   First number:  bigger means faster.
-    #   Second number: bigger means slower.
-    window.tracer(1, 1)
-
     jump_and_move_turtle(100, 50, 200, -100)
+
     turtle = rg.SimpleTurtle('square')
     draw_many_squares(turtle, 3, 75, 15)
+
     turtle3()
 
-    # When the TODOs ask you to test YOUR code, put YOUR tests here:
+    ###########################################################################
+    # When the TODOs ask you to test YOUR code, put YOUR tests below this:
+    ###########################################################################
 
-
-    window.close_on_mouse_click()
+    window.close_on_mouse_click()  # Your code must be ABOVE this statement
 
 
 def jump_and_move_turtle(x1, y1, x2, y2):
@@ -93,16 +96,16 @@ def jump_and_move_turtle(x1, y1, x2, y2):
     Jumps that SimpleTurtle (without drawing) to (x1,  y1),
     then moves that Turtle (while drawing) to (x2, y2).
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Students:
-    #   Do NOT touch this function - it has no TO DO in it.
+    #   Do NOT touch this function - it has no TO-DO in it.
     #   Do NOT copy code from this function.
     #
     # Instead, ** CALL ** this function as needed in the other problems.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     jumper = rg.SimpleTurtle()
     jumper.pen = rg.Pen('magenta', 20)
-    jumper.speed = 1
+    jumper.speed = 3
 
     jumper.pen_up()
     jumper.go_to(rg.Point(x1, y1))
@@ -128,16 +131,21 @@ def draw_many_squares(my_turtle, number_of_squares, size, twist):
       :type  size:               int
       :type  twist:              int
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Students:
-    #   Do NOT touch this function - it has no TO DO in it.
+    #   Do NOT touch this function - it has no TO-DO in it.
     #   Do NOT copy code from this function.
     #
     # Instead, ** CALL ** this function as needed in the other problems.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    old_speed = my_turtle.speed
+    my_turtle.speed = 10
+
     for _ in range(number_of_squares):
         my_turtle.draw_square(size)
         my_turtle.left(twist)
+
+    my_turtle.speed = old_speed
 
 
 def turtle3():
@@ -145,13 +153,14 @@ def turtle3():
     Constructs a classic SimpleTurtle and asks it to draw a
       "ball on pole" shape.
     """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Students:
-    #   Do NOT touch this function - it has no TO DO in it.
-    # ------------------------------------------------------------------
+    #   Do NOT touch this function - it has no TO-DO in it.
+    # -------------------------------------------------------------------------
     maja = rg.SimpleTurtle()
     maja.pen = rg.Pen('green', 10)
     maja.paint_bucket = rg.PaintBucket('black')
+    maja.speed = 15
 
     maja.right(135)
     maja.forward(300)
@@ -171,35 +180,34 @@ def try_methods():
       -- forward    50 units
       -- backward  100 units
     """
-    ####################################################################
-    # TODO: 3. Implement this function, per its doc-string above.
-    #    Put a statement in   main   to test this function
-    #    (by calling this function).
-    ####################################################################
+    ###########################################################################
+    # TODO: 3. Implement and this function, per its doc-string above.
+    #     (To test it, put a statement in   main   that calls this function.)
+    ###########################################################################
 
 
 def try_functions():
+    # IMPORTANT: Read the NOTE below before you try to solve this TO-DO!
     """
     Causes several SimpleTurtles to do the following:
      -- One jumps to (200, 100), then moves (while drawing) to (300, 30)
      -- One jumps to (100, 200), then moves (while drawing) to (0, 0)
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
-    ####################################################################
-    # TODO: 4. Implement this function, per its doc-string above.
-    #    Put a statement in   main   to test this function
-    #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
-    #    Keep reading the rest of this TO DO before doing the above!
+    ###########################################################################
+    # TODO: 4. Implement and test this function, per its doc-string above.
+    #    (To test it, put a statement in   main   that calls this function.)
     #
-    # IMPORTANT: This function requires
-    #   ** exactly 3 lines **
-    # If you think it needs more, ** ASK FOR HELP. **
-    # HINT: see   jump_and_move_turtle   above.
+    #    NOTE: This function requires
+    #      ** exactly 3 lines **
+    #    If you think it needs more, ** ASK FOR HELP. **
+    #    HINT: see   jump_and_move_turtle   above.
     #
-    ####################################################################
+    ###########################################################################
 
 
 def try_methods_and_functions():
+    # IMPORTANT: Read the NOTE below before you try to solve this TO-DO!
     """
     Constructs a SimpleTurtle and sets its   pen  to a new rg.Pen
     that is 'blue' with thickness 5.
@@ -231,20 +239,19 @@ def try_methods_and_functions():
 
       8. Draw a SQUARE whose sides are each of length 50.
     """
-    ####################################################################
-    # TODO: 5. Implement this function, per its doc-string above.
-    #    Put a statement in   main   to test this function
-    #    (by calling this function).  IMPORTANT, IMPORTANT, IMPORTANT:
-    #    Keep reading the rest of this TO DO before doing the above!
+    ###########################################################################
+    # TODO: 5. Implement and test this function, per its doc-string above.
+    #    (To test it, put a statement in   main   that calls this function.)
     #
-    # IMPORTANT: This function should ** CALL ** the
-    #   draw_many_squares
-    # function defined above.  If you don't see why, ** ASK FOR HELP. **
+    #   NOTE: This function should ** CALL ** the
+    #     draw_many_squares
+    #   function defined above.  If you don't see why, ** ASK FOR HELP. **
     #
-    ####################################################################
+    ###########################################################################
 
 
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 main()
