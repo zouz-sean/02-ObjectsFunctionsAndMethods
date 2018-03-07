42c60e7b38f8627d04809620d77bde6b2aca35c7
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-07 07:43:32 -0500

    Cosmetic changes to the instructions. No changes to content.

--- src/m5r_using_rosegraphics_objects.py
+++ src/m5r_using_rosegraphics_objects.py
@@ -29,24 +29,30 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
 #     c. What is the default height of a RoseWindow?
-#        (Type the code shown in your answer above within main to see if PyCharm will help you figure out the answer.
-#         Hint: After you type the ( in the line of code, if you wait a moment PyCharm will add the ) and has a popup.)
+#          Type into  main  the code shown in your answer above.  That will
+#          ask PyCharm will help you figure out the answer to this question.
+#          Hint: After you type the   (   in the line of code,
+#          if you wait a moment PyCharm will add the   )   and has a popup.
 #            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
-#     d. Write a line of code that construct a RoseWindow object whose height is 100 with any width you choose.
-#         (again try to use PyCharm's hints to help you figure it out)
-#            WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#     d. Write a line of code that constructs a RoseWindow object whose
+#        height is 100 with any width you choose.
+#           Again try to use PyCharm's hints to help you figure this out.
+#               WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
 #     e. Use the DOT trick to answer the following:
 #
-#          -- Write the names of two types of graphics objects that you can construct OTHER than Circle and Point:
+#          -- Write the names of two types of graphics objects
+#             that you can construct OTHER than Circle and Point:
 #                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
 #          -- Write the names of three METHODs that Circle objects have:
-#               (Hint: Use the circle from the example3 function below with the dot trick to let PyCharm help you. )
-#                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
+#               Hint: Use the circle from the  example3  function below with
+#               the dot trick to let PyCharm help you.
+#                  WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
-#          -- Write the names of three INSTANCE VARIABLEs that Circle objects have:
+#          -- Write the names of three INSTANCE VARIABLEs
+#             that Circle objects have:
 #                WRITE_YOUR_ANSWER_HERE,_REPLACING_THIS
 #
 #     f. What does a RoseWindow RENDER method do?
@@ -87,30 +93,30 @@ def example1():
 
 def example2():
     """ Displays two Point objects. """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Construct the window in which objects will be drawn.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     window = rg.RoseWindow()
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Construct some rg.Point objects.
     # Note: the y-axis goes DOWN from the TOP.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     point1 = rg.Point(100, 150)
     point2 = rg.Point(200, 50)
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # A RoseGraphics object is not associated with a window,
     # and hence are not drawn, until you ATTACH it to a window.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     point1.attach_to(window)
     point2.attach_to(window)
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # And they still are not DRAWN until you RENDER the window.
     # That will draw ALL the objects on the window.
     # This two-step approach is important for animation.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     window.render()
 
     window.close_on_mouse_click()
@@ -118,51 +124,51 @@ def example2():
 
 def example3():
     """ Displays a Circle and a Rectangle. """
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # RoseWindow: optionally takes its width and height.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     width = 700
     height = 400
     window = rg.RoseWindow(width, height)
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Circle: needs its center and radius.
     # Has  fill_color  instance variable.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     center_point = rg.Point(300, 100)
     radius = 50
     circle = rg.Circle(center_point, radius)
     circle.fill_color = 'green'
     circle.attach_to(window)
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # Rectangle: needs two opposite corners.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     point1 = rg.Point(100, 150)
     point2 = rg.Point(200, 50)
     rectangle = rg.Rectangle(point1, point2)
     rectangle.attach_to(window)
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # render: Draw ALL the objects attached to this window.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     window.render()
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # A Rectangle has instance variables  corner_1  and  corner2.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     corner1 = rectangle.corner_1
     corner2 = rectangle.corner_2
     print(corner1, corner2)  # You can also PRINT RoseGraphics objects.
     print(rectangle)  # See the Console for the output.
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # close_on_mouse_click: Keeps the window open until user clicks.
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     window.close_on_mouse_click()
 
 
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 main()
