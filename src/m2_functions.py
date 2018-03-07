de310178bb4a79a8954841593e27a529cfa53297
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-06 22:28:06 -0500

    Cosmetic changes to the instructions. No changes to content.

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -4,21 +4,20 @@ Practice DEFINING and CALLING
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
-"""
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-########################################################################
+###############################################################################
 #
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE and...
+# TODO: 2. PUT YOUR NAME IN THE ABOVE LINE and...
 #
-#   Allow this file to use the rosegraphics.py file by marking the src
-#   directory as a "Sources Root".  Do that by right clicking on the src folder,
-#   then selector  Mark Directory As --> Sources Root
+#   Allow this module to use the  rosegraphics.py  module by marking the
+#     src
+#   folder in this project as a "Sources Root", as follows:
 #
-#   Expand the import lines below and watch the red underlines disappear as you do that step.
-#   You will do that once for every project that uses rosegraphics so get used to it. :)
+#     In the Project window (to the left), right click on the src  folder,
+#     then select  Mark Directory As  ~  Sources Root.
 #
-#   Then run this module and look for more TO DO's later in the file.
-########################################################################
+###############################################################################
 
 import rosegraphics as rg
 import random
@@ -47,6 +46,7 @@ def turtle1():
     Makes that SimpleTurtle draw a yellow-filled circle.
     """
     ada = rg.SimpleTurtle('square')
+    ada.speed = 10
 
     ada.pen = rg.Pen('aquamarine', 30)
     ada.paint_bucket = rg.PaintBucket('yellow')
@@ -59,10 +59,11 @@ def turtle1():
 def turtle2():
     """
     Constructs a triangle SimpleTurtle.
-    Makes that SimpleTurle go to a RANDOM point,
+    Makes that SimpleTurtle go to a RANDOM point,
     draws a cool shape, and return to where it started from.
     """
     grace = rg.SimpleTurtle('triangle')
+    grace.speed = 7
 
     grace.pen = rg.Pen('blue', 15)
     grace.paint_bucket = rg.PaintBucket('magenta')
@@ -94,6 +95,7 @@ def turtle3():
     and then draw a black-filled circle.
     """
     maja = rg.SimpleTurtle()
+    maja.speed = 10
     maja.pen = rg.Pen('black', 10)
 
     maja.forward(300)
@@ -103,9 +105,9 @@ def turtle3():
     maja.end_fill()
 
 
-########################################################################
+###############################################################################
 #
-# TODO: 2.
+# TODO: 3.
 #   READ the code above.  Be sure you understand:
 #     -- How many functions are defined above?
 #           (Answer: 4)
@@ -119,7 +121,7 @@ def turtle3():
 #     -- What line of code calls the   main   function?
 #            (Answer: look at the LAST line of this module, far below.)
 #
-#     ** ASK QUESTIONS if you are uncertain. **
+#     ** ASK QUESTIONS if you are uncertain about any of the answers. **
 #
 #   RELATE what is DRAWN to the CODE above.  Be sure you understand:
 #       -- WHEN does the code in   main   run?
@@ -128,16 +130,16 @@ def turtle3():
 #                    the code in   turtle3   run?
 #       -- For each of the above, WHY does that code run when it does?
 #
-#     ** ASK QUESTIONS if you are uncertain. **
+#     ** ASK QUESTIONS if you are uncertain about any of the answers. **
 #
 #   When you believe you understand the answers
-#   to all of the above questions, change the above TO DO to DONE.
+#   to all of the above questions, change the above TO-DO to DONE.
 #
-########################################################################
+###############################################################################
 
-########################################################################
+###############################################################################
 #
-# TODO: 3.
+# TODO: 4.
 #   Define another function,
 #   immediately below the end of the definition of   turtle3   above.
 #   Name your new function   turtle4.
@@ -146,28 +148,28 @@ def turtle3():
 #   lines between function definitions, so be sure to do so.
 #
 #   Your new function should:
-#    1. Define a SimpleTurtle (as   turtle3   as other functions did).
+#    1. Define a SimpleTurtle.
 #    2. Set your SimpleTurtle's
 #               pen
 #       to a new rg.Pen with a color and thickness of your own choosing.
-#       See the COLORS.txt  file in this project for a list of legal color-names.
+#       The  COLORS.txt  file in this project has a list of legal color-names.
 #    3. Make your SimpleTurtle move around a bit.
 #
-# ----------------------------------------------------------------------
+#   ----------------------------------------------------------------------
 #           ** IMPORTANT:                 **
 #           ** Nothing fancy is required. **
 #           ** Save fancy stuff for exercises later today. **
-# ----------------------------------------------------------------------
+#   ----------------------------------------------------------------------
 #
 #   BTW, if you see a RED underline, that means that there is
 #     a SYNTAX (notation) error at that line or elsewhere.
 #   Get help as needed to fix any such errors.
 #
-########################################################################
+###############################################################################
 
-########################################################################
+###############################################################################
 #
-# TODO: 4.
+# TODO: 5.
 #   Add a line to   main   that CALLS your new function immediately
 #   AFTER  main  calls turtle1.  So:
 #     -- the SimpleTurtle from turtle1 should move,
@@ -179,13 +181,13 @@ def turtle3():
 #   If your code has errors (shows RED in the Console window)
 #   or does not do what it should, get help as needed to fix it.
 #
-########################################################################
+###############################################################################
 
-########################################################################
+###############################################################################
 #
-# TODO: 5.
-#   The previous two TODOs IMPLEMENTED a function (TO DO 3)
-#   and TESTED that function (TO DO 4).
+# TODO: 6.
+#   The previous two TODOs IMPLEMENTED a function (TO-DO 4)
+#   and TESTED that function (TO-DO 5).
 #
 #   Now implement AND test one more function, defining it immediately
 #   below the definition of your   turtle4   function.
@@ -198,28 +200,39 @@ def turtle3():
 #   set their characteristics (i.e., instance variables) as you choose,
 #   and make each move a bit.
 #
-# ----------------------------------------------------------------------
+#   ----------------------------------------------------------------------
 #           ** IMPORTANT:                 **
 #           ** Nothing fancy is required. **
 #           ** Save fancy stuff for exercises later today. **
-# ----------------------------------------------------------------------
+#   ----------------------------------------------------------------------
 #
-#  Get help as needed on this (and every!) exercise!
+#    Get help as needed on this (and every!) exercise!
 #
-#  As always COMMIT and Push your work as often as you want, but for sure after
-#  you have tested it and believe that it is correct.
-#
-#   Reminder of those steps...
-#   COMMIT your work by selecting VCS from the menu bar, then select Commit Changes
-#   Make sure only the files you want to commit are checked and optionally
-#   add a quick Commit message to describe your work.  Then hover over the
-#   Commit button and select Commit and Push.  Commit saves the work to
-#   your computer.  "and Push" saves a copy of your work up into your Github
-#   repository (saving to the cloud is a better way to permanently safe work).
-#
-########################################################################
+###############################################################################
 
-# ----------------------------------------------------------------------
+###############################################################################
+#
+# TODO: 7.
+#   COMMIT and PUSH your work (after changing this TO-DO to DONE).
+#
+#   As a reminder, here is how you should do so:
+#     1. Select   VCS   from the menu bar (above).
+#     2. Choose  Commit  from the pull-down menu that appears.
+#     3. In the  Commit Changes  window that pops up:
+#          - HOVER over the  Commit  button
+#              (in the lower-right corner of the window)
+#          - CLICK on  Commit and Push.
+#          - (if asked again to Push, select Push)
+#
+#   COMMIT adds the changed work to the version control on your computer
+#   and PUSH adds the changed work into your Github repository in the "cloud".
+#
+#    COMMIT-and-PUSH your work as often as you want, but for sure
+#    after you have tested the module and believe that it is correct.
+#
+###############################################################################
+
+# -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 main()
